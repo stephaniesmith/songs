46c33c66434ffa150b715b640bd1bde505fbce71
Stephanie Smith <stephsmithcode@gmail.com>
2019-02-27 21:34:37 -0800

    reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,14 @@
+const songsReducer = () => {
+  return [
+    { title: 'No Scrubs', duration: '4:05' },
+    { title: 'Macarena', duration: '2:30' },
+    { title: 'All Star', duration: '3:15' },
+    { title: 'I Want it That Way', duration: '1:45' }
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, { type, payload }) => {
+  if(type === 'SONG_SELECTED') return payload;
+
+  return selectedSong;
+};
