93dffaad107a4e7de9314035c1472eec872bc7f4
Stephanie Smith <stephsmithcode@gmail.com>
2019-02-28 20:37:09 -0800

    song list

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -4,16 +4,32 @@ import { connect } from 'react-redux';
 
 export class SongList extends Component {
   static propTypes = {
-
+    songs: PropTypes.array,
   };
 
   render() {
+    const { songs } = this.props;
     return (
-      <div>
-        Song List
+      <div className="ui divided list">
+        {songs.map(({ title }) => {
+          return (
+            <div className="item" key={title}>
+              <div className="right floated content">
+                <button className="ui button primary">
+                Select
+                </button>
+              </div>
+              <div className="content">{title}</div>
+            </div>
+          );
+        })}
       </div>
     );
   }
 }
 
-export default connect()(SongList);
+const mapStateToProps = ({ songs }) => {
+  return { songs };
+};
+
+export default connect(mapStateToProps)(SongList);
