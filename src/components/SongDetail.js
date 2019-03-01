3c95daaf3c55869603d3db1383068efa05ab2efd
Stephanie Smith <stephsmithcode@gmail.com>
2019-02-28 21:02:31 -0800

    song detail

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -3,10 +3,18 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 const SongDetail = ({ selectedSong }) => {
-  console.log(selectedSong);
+  if(!selectedSong) return <div>Select a song!</div>;
+  const { title, duration } = selectedSong;
+
+
   return (
     <div>
-      SONG DETAIL
+      <h3>Details for:</h3>
+      <p>
+        Title: {title}
+        <br/>
+        Duration: {duration}
+      </p>
     </div>
   );
 };
@@ -19,7 +27,7 @@ const mapStateToProps = ({ selectedSong }) => ({ selectedSong });
 
 const mapDispatchToProps = {
   
-}
+};
 
 
 export default connect(mapStateToProps)(SongDetail);
